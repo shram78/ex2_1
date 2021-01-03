b4e86f4cd6790d5d8666fdb7cb6b330edbcd872f
shram78 <ra00275@gmail.com>
2021-01-03 16:03:33 +0300

    for check

---
+++ ex2_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Напишите простейшую программу, которая выводит указанное(установленное) пользователем сообщение заданное количество раз.*/
+namespace ex2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько раз мне напечать твою мессагу? ");
+            int messageCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Какую мессагу печатать, введи текст ");
+            string teksOfMessage = Console.ReadLine();
+
+            for (int i = 0; i < messageCount; i++)
+            {
+                Console.WriteLine(teksOfMessage);
+            }
+            Console.ReadLine();
+        }
+    }
+}
